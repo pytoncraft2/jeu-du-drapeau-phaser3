bbdcc428c6dc897fc9c37a4cc20b83d012690df8
Hennequin <pythoncraft2@gmail.com>
2021-11-13 05:22:35 +0100

    [!] image disparu

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -25,11 +25,103 @@ const config = {
 };
 
 function preload() {
-  this.load.image('dessinatrice1', 'assets/fond/bg.png');
+  this.load.atlas('dessinatric1', 'assets/personnages/dessinatrice1/dessinatrice1.png', 'assets/personnages/dessinatrice1/dessinatrice1_atlas.json');
 }
 
 
 function create() {
+
+this.anims.create({
+  key: "attack1_dessinatrice1",
+  frames: this.anims.generateFrameNumbers('dessinatrice1', {
+    frames: ['profil', 'position_a1', 'position_a2', 'position_a3', 'profil','pas_jkd']
+  }),
+  frameRate: 6,
+  repeat: 0
+});
+
+this.anims.create({
+  key: "run_dessinatrice1",
+  frames: this.anims.generateFrameNumbers('dessinatrice1', {
+    frames: ['pas_jkd','pas_jkd2', 'pas_jkd4','pas_jkd3']
+  }),
+  frameRate: 6,
+  repeat: 0
+});
+
+
+this.anims.create({
+  key: "goback_dessinatrice1",
+  frames: this.anims.generateFrameNumbers('dessinatrice1', {
+    frames: ['dos5', 'dos7.8', 'dos8', 'dos9', 'dos10', 'dos11', 'dos3']
+  }),
+  frameRate: 7,
+  repeat: 0
+});
+
+
+this.anims.create({
+  key: "jump_dessinatrice1",
+  frames: this.anims.generateFrameNumbers('dessinatrice1', {
+    frames: ['jump0', 'jump1', 'jump2', 'jump3', 'jump4', 'jump5', 'jump0']
+  }),
+  frameRate: 8,
+  repeat: 0
+});
+
+
+this.anims.create({
+  key: "front_dessinatrice1",
+  frames: this.anims.generateFrameNumbers('dessinatrice1', {
+    frames: ['face5', 'face2', 'face3', 'face4', 'face1']
+  }),
+  frameRate: 6,
+  repeat: 0
+});
+this.anims.create({
+  key: "walk_dessinatrice1",
+  frames: this.anims.generateFrameNumbers('dessinatrice1', {
+    frames: ['profil_jkd11', 'profil_jkd13', 'profil_jkd14', 'profil_jkd15','profil_jkd8']
+  }),
+  frameRate: 5,
+  repeat: 0
+});
+
+this.anims.create({
+  key: "attack1_naruto",
+  frames: this.anims.generateFrameNumbers('naruto', {
+    frames: ['profil', 'position_a1', 'position_a2', 'position_a3', 'profil']
+  }),
+  frameRate: 6,
+  repeat: 0
+});
+
+this.anims.create({
+  key: "goback_naruto",
+  frames: this.anims.generateFrameNumbers('naruto', {
+    frames: ['dos5', 'dos7.8', 'dos8', 'dos9', 'dos10', 'dos11', 'dos3']
+  }),
+  frameRate: 7,
+  repeat: 0
+});
+
+this.anims.create({
+  key: "front_naruto",
+  frames: this.anims.generateFrameNumbers('naruto', {
+    frames: ['face1', 'face2', 'face3', 'face4', 'face5', 'face7']
+  }),
+  frameRate: 6,
+  repeat: 0
+});
+this.anims.create({
+  key: "walk_naruto",
+  frames: this.anims.generateFrameNumbers('naruto', {
+    frames: ['profil3', 'profil4', 'profil5', 'profil6']
+  }),
+  frameRate: 5,
+  repeat: 0
+});
+
   const self = this;
   this.players = {}
   this.players['Naruto'] = this.add.group();
--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const jsdom = require('jsdom');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const datauri = require('datauri');
-const { JSDOM } = require('jsdom');
+
+// const io = require('socket.io').listen(server);
+const Datauri = require('datauri');
+
+const datauri = new Datauri();
+const { JSDOM } = jsdom;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -28,7 +33,7 @@ function setupAuthoritativePhaser() {
     };
     dom.window.URL.revokeObjectURL = (objectURL) => {};
     dom.window.gameLoaded = () => {
-      server.listen(8000, function () {
+      server.listen(8082, function () {
         console.log(`Listening on ${server.address().port}`);
       });
     };
@@ -39,3 +44,51 @@ function setupAuthoritativePhaser() {
 }
 
 setupAuthoritativePhaser();
+
+
+
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+// const Datauri = require('datauri');
+//
+// const datauri = new Datauri();
+// const { JSDOM } = jsdom;
+// // const datauri = require('datauri');
+// // const { JSDOM } = require('jsdom');
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// function setupAuthoritativePhaser() {
+//   JSDOM.fromFile(path.join(__dirname, 'authoritative_server/index.html'), {
+//     // To run the scripts in the html file
+//     runScripts: "dangerously",
+//     // Also load supported external resources
+//     resources: "usable",
+//     // So requestAnimatinFrame events fire
+//     pretendToBeVisual: true
+//   }).then((dom) => {
+//     dom.window.URL.createObjectURL = (blob) => {
+//       if (blob){
+//         return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+//       }
+//     };
+//     dom.window.URL.revokeObjectURL = (objectURL) => {};
+//     dom.window.gameLoaded = () => {
+//       server.listen(8000, function () {
+//         console.log(`Listening on ${server.address().port}`);
+//       });
+//     };
+//     dom.window.io = io;
+//   }).catch((error) => {
+//     console.log(error.message);
+//   });
+// }
+//
+// setupAuthoritativePhaser();
