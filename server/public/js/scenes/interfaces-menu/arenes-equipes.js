9cebe61b1384934ffd0b15a3fbbbe5b7be4c03c0
Hennequin <pythoncraft2@gmail.com>
2021-11-18 22:38:44 +0100

    [~] améliroation feu d'artifice

--- server/public/js/scenes/interfaces-menu/arenes-equipes.js
+++ server/public/js/scenes/interfaces-menu/arenes-equipes.js
@@ -40,27 +40,28 @@ const AreneEquipes = new Phaser.Class({
      var spark0 = this.add.particles('bullet').setDepth(200);
      var spark1 = this.add.particles('bulletVerte').setDepth(200);
 
+
      for (var i = 0; i < points.length; i++)
-     {
-         var p = points[i];
+{
+    var p = points[i];
 
-         tempVec.copy(tangents[i]).normalizeRightHand().scale(-32).add(p);
+    tempVec.copy(tangents[i]).normalizeRightHand().scale(-32).add(p);
 
-         var angle = Phaser.Math.RadToDeg(Phaser.Math.Angle.BetweenPoints(p, tempVec));
+    var angle = Phaser.Math.RadToDeg(Phaser.Math.Angle.BetweenPoints(p, tempVec));
 
-         var particles = (i % 2 === 0) ? spark0 : spark1;
+    var particles = (i % 2 === 0) ? spark0 : spark1;
 
-         particles.createEmitter({
-             x: tempVec.x,
-             y: tempVec.y,
-             angle: angle,
-             speed: { min: -10, max: 50 },
-             gravityY: 200,
-             scale: { start: 0.4, end: 0.1 },
-             lifespan: 800,
-             blendMode: 'SCREEN'
-         });
-     }
+    particles.createEmitter({
+        x: tempVec.x,
+        y: tempVec.y,
+        angle: angle,
+        speed: { min: -100, max: 500 },
+        gravityY: 200,
+        scale: { start: 0.4, end: 0.1 },
+        lifespan: 800,
+        blendMode: 'SCREEN'
+    });
+}
      /*
      var particles = this.add.particles('bullet');
 
@@ -95,7 +96,23 @@ const AreneEquipes = new Phaser.Class({
     var up = 90;
     var top = 400;
 
-    this.equipe = this.add.text(300, 120, ['CHOISIR UNE ARENE ET VOTRE EQUIPE']).setFontSize(47).setFontFamily('Trebuchet MS').setColor('#6badce').setShadow(2, 2, "white", 2, true, true);
+    // this.equipe = this.add.text(300, 120, ['CHOISIR UNE ARENE ET VOTRE EQUIPE']).setFontSize(47).setFontFamily('Trebuchet MS').setColor('#6badce').setShadow(2, 2, "white", 2, true, true);
+    this.victoire = this.add.text(287, 380, ['VICTOIRE']).setFontSize(47).setFontFamily('Trebuchet MS').setColor('#2eed12').setShadow(2, 2, "white", 2, true, true);
+    this.defaite = this.add.text(900, 380, ['DEFAITE']).setFontSize(47).setFontFamily('Trebuchet MS').setColor('#6badce').setShadow(2, 2, "white", 2, true, true);
+    alert()
+    var emitter0 = this.add.particles('bullet').createEmitter({
+    x: 900 + this.defaite.displayWidth / 2,
+    alpha: 0.4,
+    tint: 0xd00b0b,
+    y: 380 + this.defaite.displayHeight / 2,
+    speed: { min: -800, max: 800 },
+    angle: { min: 0, max: 360 },
+    scale: { start: 0.5, end: 0 },
+    blendMode: 'SCREEN',
+    //active: false,
+    lifespan: 600,
+    gravityY: 800
+});
     //
     // new Arene.Naruto(this, up + 400, top, halfHeight, halfWidth, this.personnage);
     // new Arene.Pikachu(this, up + 800, top, halfHeight, halfWidth, this.personnage);
