2364ce7c66663fbf784034537644a94576850066
Hennequin <pythoncraft2@gmail.com>
2021-11-19 03:57:44 +0100

    [~]

--- server/public/js/scenes/interfaces-menu/main-menu-scene.js
+++ server/public/js/scenes/interfaces-menu/main-menu-scene.js
@@ -18,10 +18,8 @@ const MainMenu = new Phaser.Class({
         const halfHeight = 720 / 2;
 
         this.goSelectionM = this.add.text(halfWidth - 90, halfHeight - 10, ['JOUER']).setFontSize(47).setFontFamily('Trebuchet MS').setColor('#6badce').setShadow(2, 2, "white", 2, true, true);
-        // this.fullscreen = this.add.text(halfWidth - 90, halfHeight - 10, ['Activer pleine ecran']).setFontSize(27).setFontFamily('Trebuchet MS').setColor('#6badce');
         this.fullscreen = this.add.image(40,40, 'fullscreen')
 
-        // Multijoueur
         this.goSelectionM.setInteractive().on('pointerdown', function() {
           self.scene.start('selectionJoueur');
         });
