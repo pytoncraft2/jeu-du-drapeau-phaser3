3c0920c3d77e563b8f83d6444c3e7c7fcded496b
Hennequin <pythoncraft2@gmail.com>
2021-11-28 01:56:01 +0100

    [~]

--- server/public/js/scenes/arene.js
+++ server/public/js/scenes/arene.js
@@ -291,7 +291,7 @@ const Arene = new Phaser.Class({
     this.fontaine2 = this.add.image(8235, -1553, 'fontaine').setDepth(2)
     this.fontaine2Derriere = this.add.image(8235, -1553, 'fontaineDerriere').setDepth(0)
 
-    this.drapeauVert = this.add.image(8443.85357152924,-1883.7104390337054, 'drapeauVert').setDepth(1)
+    // this.drapeauVert = this.add.image(8443.85357152924,-1883.7104390337054, 'drapeauVert').setDepth(1)
 
     let maison2 = this.add.group()
     maison2.addMultiple([interieurMaison2, facade2, toit2]); // array of game objects
