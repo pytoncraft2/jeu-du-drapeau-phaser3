18caaa1b19df762f2ecd806a789890258aac5d95
pytoncraft2 <pythoncraft2@gmail.com>
2022-02-19 23:22:31 +0100

    [+] colyseus serveur dossier test

---
+++ src/client/game/config.ts
@@ -0,0 +1,17 @@
+import Phaser from "phaser"
+
+export default {
+  type: Phaser.AUTO,
+  parent: "game",
+  backgroundColor: "#33A5E7",
+  scale: {
+    width: 640,
+    height: 320,
+    mode: Phaser.Scale.NONE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  dom: {
+    createContainer: true
+  },
+  pixelArt: true,
+}
---
+++ src/server/game/config.ts
@@ -0,0 +1,22 @@
+import Phaser from "phaser"
+import Game from "./scenes/game"
+import Lobby from "./scenes/lobby"
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.HEADLESS,
+  parent: "game",
+  scale: {
+    width: 640,
+    height: 320,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 800 },
+    },
+  },
+  scene: [Lobby, Game],
+  fps: { min: 10, target: 60 },
+}
+
+export default config
