0666b1cc5f5b5d780975aa144d3e071dcf96c6b0
pytoncraft2 <pythoncraft2@gmail.com>
2022-03-10 23:27:10 +0100

    [⚙️][⚙️] coté client mise a jour du texte

--- src/client/game/scenes/Lobby.ts
+++ src/client/game/scenes/Lobby.ts
@@ -36,7 +36,7 @@ export default class Lobby extends Phaser.Scene {
   }
 
   copieUrl() {
-    let url = `${window.location.protocol}//${document.location.hostname}:${document.location.port}/${this.salon}` 
+    let url = `${window.location.protocol}//${document.location.hostname}:${document.location.port}/${this.salon}`
     navigator.clipboard.writeText(url).then(function() {
       console.log(`${url} copié!`);
     }, function() {
@@ -130,8 +130,36 @@ export default class Lobby extends Phaser.Scene {
           contenu.push(val.concat(`${joueursPresents[val].pret ? '  ✅ PRET !' : ' 🔴 CHOIX EN COURS...'}`))
           console.log(joueursPresents[val].indexConfirmation)
 
-          // self.container.getAll().forEach((img, l) => {
-          // })
+          self.container.getAll().forEach((img, l) => {
+          })
+          const imgs = self.container.getAll()[joueursPresents[val].indexConfirmation] as any;
+          const nouveau = self.container.getAll()[joueursPresents[val].indexConfirmation] as any;
+          const ancien = self.container.getAll()[joueursPresents[val].ancienIndexConfirmation] as any;
+
+          // console.log(numbers[numbers.length - 2]);
+          // console.log('ancien ->>')
+          // console.log(ancien)
+          // console.log('nouveau ->>')
+          // console.log(nouveau)
+          // console.log(imgs)
+          //image ou l'on clique
+
+          if (nouveau) {
+            if (!nouveau.texte) {
+              nouveau.texte = self.add.text(nouveau.x, nouveau.y + nouveau.displayHeight / 2 + 30, [val], { fontFamily: 'CustomFontNormal' }).setFontSize(20).setAlpha(0.5).setOrigin(0.5)
+              self.container.add([nouveau.texte])
+            } else {
+              // imgs.texte.setText(['OK'])
+            }
+          }
+
+          // if (ancien) {
+          //   ancien.texte.setText('coucou')
+          // }
+
+          if (joueursPresents[val].indexConfirmation !== -1) {
+            self.container.add([imgs.texte])
+          }
         })
 
         self.panelGauche.setTitre(`Joueurs : ${Object.keys(joueursPresents).length} / 4`)
