d7656251d6c6098c96cd3d03c963f15252922412
pytoncraft2 <pythoncraft2@gmail.com>
2022-03-11 17:38:22 +0100

    [🧹]

--- src/server/game/LobbyState.ts
+++ src/server/game/LobbyState.ts
@@ -21,19 +21,6 @@ export class ListeJoueurIndex extends Schema {
   @type("string") liste: string
 }
 
-// class Player {
-//     @type("x")
-//     width: number;
-//
-//     @type("y")
-//     height: number;
-// }
-//
-// class MyState extends Schema {
-//     @type({ map: Player })
-//     players = new MapSchema<Player>();
-// }
-
 export class Input extends Schema {
   @type("boolean") left: boolean = false
   @type("boolean") right: boolean = false
@@ -41,15 +28,9 @@ export class Input extends Schema {
   @type("boolean") down: boolean = false
 }
 
-// export class JoueurIndex extends Schema {
-//   @type("string") joueurIndex = new ArraySchema<string>();
-// }
 
 export class LobbyState extends Schema {
   //@ts-ignore
   @type({ map: Joueur }) joueurs = new MapSchema<Joueur>()
-  // @type({ map: ListeJoueurIndex }) joueurIndexs = new MapSchema<ListeJoueurIndex>();
   @type([ "string" ]) listeJoueurIndex = new ArraySchema<string>();
-
-  // @type({ map: JoueurIndex }) joueurIndexs = new MapSchema<JoueurIndex>();
 }
