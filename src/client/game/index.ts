3dbcbf979312bdd0a39fb4a0343b5c201fe5f145
pytoncraft2 <pythoncraft2@gmail.com>
2022-03-18 21:44:31 +0100

    [+] responsive

--- src/client/game/index.ts
+++ src/client/game/index.ts
@@ -1,47 +1,31 @@
 import Phaser from "phaser"
-import config from "./config"
 import Scenes from "./scenes/index"
 
-// const config =  {
-//   type: Phaser.AUTO,
-//   parent: "game",
-//   transparent: true,
-//   autoFocus: true,
-//   scale: {
-//     mode: Phaser.Scale.CENTER_BOTH,
-//     autoCenter: Phaser.Scale.CENTER_BOTH,
-//     width: window.innerWidth,
-//     height: window.innerHeight
-//   },
-//   callbacks: {
-//     postBoot: () => {
-//       // game.scale.refresh();
-//     },
-//   },
-//   // canvasStyle: `display: block; width: 100%; height: 100%;`,
-//   dom: {
-//     createContainer: true
-//   },
-//   audio: {
-//     disableWebAudio: false,
-//   }
-//
-// }
-
+const config = {
+  type: Phaser.AUTO,
+  parent: "game",
+  transparent: true,
+  scale: {
+    mode: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+    callbacks: {
+      postBoot: () => {
+        game.scale.refresh();
+      },
+    },
+  dom: {
+    createContainer: true
+  },
+  audio: {
+    disableWebAudio: true,
+  }
+}
 
 const game=new Phaser.Game(
   Object.assign(config, {
     scene: Scenes,
   })
 )
-
-// window.addEventListener('resize', () => {
-//   game.scale.refresh();
-//   setTimeout(() => {
-//   game.scale.resize(window.innerWidth, window.innerHeight);
-//   game.canvas.setAttribute(
-//     'style',
-//     `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
-//   );
-// }, 100);
-// });
