170ca9281454f988a89c8fee61bd1df962d8a974
pytoncraft2 <pythoncraft2@gmail.com>
2022-03-18 17:25:49 +0100

    [~] test responsive

--- src/client/game/index.ts
+++ src/client/game/index.ts
@@ -1,6 +1,37 @@
 import Phaser from "phaser"
-import config from "./config"
+// import config from "./config"
 import Scenes from "./scenes/index"
+
+interface Window {
+  sizeChanged: () => void;
+  game: Phaser.Game;
+}
+
+const config =  {
+  type: Phaser.AUTO,
+  parent: "game",
+  transparent: true,
+  autoFocus: true,
+  scale: {
+    mode: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+  callbacks: {
+    postBoot: () => {
+      game.scale.refresh();
+    },
+  },
+  canvasStyle: `display: block; width: 100%; height: 100%;`,
+  dom: {
+    createContainer: true
+  },
+  audio: {
+    disableWebAudio: false,
+  }
+
+}
 
 
 const game=new Phaser.Game(
@@ -11,4 +42,12 @@ const game=new Phaser.Game(
 
 window.addEventListener('resize', () => {
   game.scale.refresh();
+  console.log("RRRRRRRRRRRESSIZE")
+  setTimeout(() => {
+  game.scale.resize(window.innerWidth, window.innerHeight);
+  game.canvas.setAttribute(
+    'style',
+    `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
+  );
+}, 100);
 });
