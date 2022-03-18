3fe9d44275bb2047592d598da597948d8884cb53
pytoncraft2 <pythoncraft2@gmail.com>
2022-03-18 17:33:51 +0100

    [~] retour config normal

--- src/client/game/config.ts
+++ src/client/game/config.ts
@@ -1,5 +1,16 @@
 import Phaser from "phaser"
 
-
-
-// window.game = new Game(gameConfig);
+export default {
+  type: Phaser.AUTO,
+  parent: "game",
+  transparent: true,
+  scale: {
+    mode: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+  dom: {
+    createContainer: true
+  }
+}
